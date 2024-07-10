2ad6f205cd3335503528a8fd5b85ca10d575cde0
Angie <ahynk9@gmail.com>
2024-07-10 16:13:44 +0800

    add angie

---
+++ medication.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medications</title>
+    <link rel="icon" href="resources/site-logo.png" type="image/png"> <!-- INSERT SITE LOGO -->
+    <!-- IMPORT FILES -->
+    <link rel="stylesheet" href="medication.css">
+</head>
+
+<body>
+    <aside class="sidebar">
+        <div class="sidebar-navtop">
+            <p><a href="homepage.php"><img src="resources/back-icon.png" alt="Go Back to Homepage Icon"></a></p>
+            <p><a href="userdashboard.php"><img src="resources/dashboard-icon.png" alt="User Dashboard Icon"></a></p>
+            <p><a href="appointment.php"><img src="resources/calendar-icon.png" alt="Appointment Icon"></a></p>
+            <p><a href="profilepage.php"><img src="resources/profile-icon.png" alt="Profile Icon"></a></p>
+            <p><a href="medication.php"><img src="resources/medication-icon.png" alt="Medication Icon"></a></p>
+            <p><a href="medicalreport.php"><img src="resources/medreport-icon.png" alt="Medical Report Icon"></a></p>
+            <p><a href="#"><img src="resources/settings-icon.png" alt="Settings Icon"></a></p>
+        </div>
+        <div class="sidebar-navbottom">
+            <p><a href="#"><img src="resources/signout-icon.png" alt="Sign Out Icon"></a></p>
+        </div>
+    </aside>
+
+    <main>
+        <div class="past-appointment-prescriptions-container">
+            <h2>Past Appointment Prescriptions</h2>
+            <div class="scroller">
+                <div class="past-appointment-prescription-card">    
+                    <h2>21 June 2024 | Cough and Flu</h2>
+                    <hr class="horizontal-divider">
+                    <div class="card-content">
+                        <div class="left">
+                            <p><strong>Type: </strong>General Consultation</p>
+                            <p><strong>Doctor: </strong>Dr. Amalina Skridri</p>
+                            <p><strong>Symptoms Log: </strong></p>
+                            <ul>
+                                <li>Shortness of Breath</li>
+                                <li>Chest Pain</li>
+                                <li>Blocked Nose</li>
+                            </ul>
+                            <p><strong>Notes: </strong></p>
+                            <p>Been having these symptoms for one week</p>
+                        </div>
+                        <hr class="vertical-divider">
+                        <div class="right">
+                            <p><strong>Prescription: </strong></p>
+                            <p><strong>Bromhexine | 8 mg</strong></p>
+                            <p>Twice daily (morning and evening)</p>
+                            <p>Before food, take as required</p>
+                            <p><strong>Dextromethorphan | 30 mg</strong></p>
+                            <p>Every 6-8 hours as needed</p>
+                            <p>Before food, chew before swallowing</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="past-appointment-prescription-card">    
+                    <h2>13 April 2024  |  Gastritis</h2>
+                    <hr class="horizontal-divider">
+                    <div class="card-content">
+                        <div class="left">
+                            <p><strong>Type: </strong>Emergency Walk-in</p>
+                            <p><strong>Doctor: </strong>Dr. Khairul Ali Khan</p> <!-- check point --> 
+                            <p><strong>Symptoms Log: </strong></p>
+                            <ul>
+                                <li>Stomach Bloating</li>
+                                <li>Heartburn</li>
+                                <li>Bleching</li>
+                            </ul>
+                            <p><strong>Notes: </strong></p>
+                            <p>Started experiencing all listed symptoms after eating spicy food.</p>
+                        </div>
+                        <hr class="vertical-divider">
+                        <div class="right">
+                            <p><strong>Prescription: </strong></p>
+                            <p><strong>Famotidine | 40 mg</strong></p>
+                            <p>Thrice daily (morning, noon and evening)</p>
+                            <p>After food, take as required</p>
+                            <p><strong>Alumag | 250 mg</strong></p>
+                            <p>Take as required, at most twice a day</p>
+                            <p>After food, chew before swallowing</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="past-appointment-prescription-card">    
+                    <h2>05 January 2024  |  Food Poisoning</h2>
+                    <hr class="horizontal-divider">
+                    <div class="card-content">
+                        <div class="left">
+                            <p><strong>Type: </strong>Outpatient</p>
+                            <p><strong>Doctor: </strong>Dr. Terrance Goh</p> <!-- check point --> 
+                            <p><strong>Symptoms Log: </strong></p>
+                            <ul>
+                                <li>Diarrhea</li>
+                                <li>Stomachache</li>
+                                <li>Nausea</li>
+                            </ul>
+                            <p><strong>Notes: </strong></p>
+                            <p>Just came back from travels in Indonesia. Already went to the doctors before flying back but symptoms still persisted.</p>
+                        </div>
+                        <hr class="vertical-divider">
+                        <div class="right">
+                            <p><strong>Prescription: </strong></p>
+                            <p><strong>Oral electrolytes | 10 packets</strong></p>
+                            <p>Mix thoroughly with 250ml of water</p>
+                            <p>Take as required, suggested to finish</p>
+                            <p><strong>Saccharomyces boulardii | 250 mg</strong></p>
+                            <p>Take twice daily as instructed</p>
+                            <p>Must finished the course of medication</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
--- userdashboard.php
+++ userdashboard.php
@@ -1,1 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Dashboard</title>
+    <link rel="icon" href="resources/site-logo.png" type="image/png"> <!-- INSERT SITE LOGO -->
+    <!-- IMPORT FILES -->
+    <link rel="stylesheet" href="userdashboard.css">
+</head>
 
+<body>
+    <aside class="sidebar">
+        <div class="sidebar-navtop">
+            <p><a href="homepage.php"><img src="resources/back-icon.png" alt="Go Back to Homepage Icon"></a></p>
+            <p><a href="userdashboard.php"><img src="resources/dashboard-icon.png" alt="User Dashboard Icon"></a></p>
+            <p><a href="appointment.php"><img src="resources/calendar-icon.png" alt="Appointment Icon"></a></p>
+            <p><a href="profilepage.php"><img src="resources/profile-icon.png" alt="Profile Icon"></a></p>
+            <p><a href="medication.php"><img src="resources/medication-icon.png" alt="Medication Icon"></a></p>
+            <p><a href="medicalreport.php"><img src="resources/medreport-icon.png" alt="Medical Report Icon"></a></p>
+            <p><a href="#"><img src="resources/settings-icon.png" alt="Settings Icon"></a></p>
+        </div>
+        <div class="sidebar-navbottom">
+            <p><a href="#"><img src="resources/signout-icon.png" alt="Sign Out Icon"></a></p>
+        </div>
+    </aside>
+
+    <main>
+        <div class="user-greetings-container">
+            <h2>Hi, Abdulla Safar</h2>
+            <h1>Welcome Back!</h1>
+        </div>
+        <div class="checkup-reminder-container">
+            <div class="checkup-reminder-left">
+                <h4>Reminder</h4>
+                <h1>Have You Had a Routine Health Check this Month?</h1>
+                <p>Book your routine health check now!</p>
+                <div class="checkup-reminder-button">
+                    <button class="check-now-button">Check Now</button>
+                    <button class="view-report-button">View Report</button>
+                </div>
+            </div>
+            <div class="checkup-reminder-right">
+                <img src="resources/doctor-illustration.png" alt="Illustration of a Doctor">
+            </div> 
+        </div>
+        <div class="bottom-row">
+            <div class="upcoming-checkup-container">
+                <h2>Upcoming Checkup</h2>
+                <div class="scroller">
+                    <div class="upcoming-checkup-list">    
+                        <div class="list-date">
+                            <h3>11 Jul</h3>
+                            <h3>9am</h3>
+                        </div>
+                        <div class="list-details">
+                            <h3>Blood Test</h3>
+                            <p>Subang Jaya Medical Centre</p>
+                        </div>
+                    </div>
+                    <div class="upcoming-checkup-list">    
+                        <div class="list-date">
+                            <h3>23 Sept</h3>
+                            <h3>2pm</h3>
+                        </div>
+                        <div class="list-details">
+                            <h3>MRI Scan</h3>
+                            <p>Sunway Medical Centre</p>
+                        </div>
+                    </div>
+                    <div class="upcoming-checkup-list">    
+                        <div class="list-date">    
+                            <h3>30 Nov</h3>
+                            <h3>11am</h3>
+                        </div>
+                        <div class="list-details">
+                            <h3>Thyroid Function Test</h3>
+                            <p>UNI KL Clinic</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="last-checkup-container">
+                <h2>Your Last Health Check</h2>
+                <div class="scroller">
+                    <div class="last-checkup-list">    
+                        <img src="resources/kidney-checkup-icon.png" alt="Kidney Checkup Icon">
+                        <div class="list-details">
+                            <h3>Regular Kidney Check</h3>
+                            <p>09 October 2023</p>
+                        </div>
+                    </div>
+                    <div class="last-checkup-list">    
+                        <img src="resources/dental-checkup-icon.png" alt="Dental Checkup Icon">
+                        <div class="list-details">
+                            <h3>Dental Health</h3>
+                            <p>18 January 2024</p>
+                        </div>
+                    </div>
+                    <div class="last-checkup-list">    
+                        <img src="resources/heart-checkup-icon.png" alt="Heart Checkup Icon">
+                        <div class="list-details">
+                            <h3>Heart Stress Test</h3>
+                            <p>21 August 2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
