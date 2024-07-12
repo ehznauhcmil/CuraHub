f4b30eb08d5d4c3d4a1f14f64e8531ef1d4c9f80
ehznauhcmil <135931394+ehznauhcmil@users.noreply.github.com>
2024-07-12 19:13:57 +0800

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "profile_management";
+$dbname = "curahub";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
